d111e007cdd9f3a68b37717b5b5e3cd3f777dad3
StitchNG <stitchnig@gmail.com>
2019-01-02 16:54:26 +0100

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/**
+ * adonis-infobip
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com> | Aziz Abdul <>
+ */
+
+const path = require('path')
+
+module.exports = async function (cli) {
+  await cli.makeConfig('infobip.js', path.join(__dirname, './config/infobip.js'))
+    .catch((e) => {})
+    
+  cli.command.completed('create', 'config/infobip.js')
+}
