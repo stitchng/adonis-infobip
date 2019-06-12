cba86c6b2343c5345514a8dece0385b42397c8b1
Ifeora Okechukwu <isocroft@gmail.com>
2019-06-12 10:08:48 +0100

    chore(files): setting up project files

--- providers/InfoBipProvider.js
+++ providers/InfoBipProvider.js
@@ -6,9 +6,13 @@ class InfoBipProvider extends ServiceProvider {
   register () {
     this.app.singleton('Adonis/Addons/InfoBip', () => {
       const Config = this.app.use('Adonis/Src/Config')
-      const InfoBipHelper = require('../src/InfoBip')
-      return new InfoBipHelper(require('got'), require('lodash'), Config)
+      const Env = this.app.use('Env')
+      const InfoBipAPIClient = require('../src/InfoBip/index.js')
+      
+      return new InfoBipAPIClient(require('infobip-node'), Config, Env)
     })
+
+    this.app.alias('Adonis/Addons/InfoBip', 'InfoBip')
   }
 }
 
--- src/InfoBip/index.js
+++ src/InfoBip/index.js
@@ -1,1 +1,16 @@
+'use strict'
 
+class InfoBipApiClient {
+
+    constructor(Agent, Config, Env){
+
+        let isProd = (Env.get('NODE_ENV') === "production")
+        this.agent = new Agent(Config.get('infobip.apiKey'), isProd, {
+            authType: Config.get('infobip.authType'), 
+            username: Config.get('infobip.username'), 
+            password: Config.get('infobip.password')
+        })
+    }
+}
+
+module.exports = InfoBipApiClient
